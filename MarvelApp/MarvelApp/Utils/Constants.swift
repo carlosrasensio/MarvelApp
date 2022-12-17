0dd9b93536a91faf8c22f52cc4a8f08521c6b649
carlosrasensio <carlosrasensio@gmail.com>
2022-12-17 18:31:56 +0100

    Modified Marvel API public key.

--- MarvelApp/MarvelApp/Utils/Constants.swift
+++ MarvelApp/MarvelApp/Utils/Constants.swift
@@ -15,8 +15,8 @@ struct Constants {
         static let version = 1
         static let type = "public"
         static let timeStamp = Date().timeIntervalSince1970
-        static let publicApiKey = "bdd7f1b10e8f1b342807f0d5a01f3e03"
         static let privateApiKey = ""   // TODO: Insert private api key
+        static let publicApiKey = "62c59e290474b734d02ee4ffe458df41"
         static let limit = 50
 
         struct URLs {
